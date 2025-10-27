20f46730424e149dd3610970858c60a990b79410
pastelnata <ritabb04.05@gmail.com>
2025-10-27 18:32:41 +0100

    add prettier to workflow & format code

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig, env } from "prisma/config";
-import dotenv from "dotenv";
+import { defineConfig, env } from 'prisma/config';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
-  engine: "classic",
+  engine: 'classic',
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env('DATABASE_URL'),
   },
 });
