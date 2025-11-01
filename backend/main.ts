7f6a3210b1e6e8b40697fa51fecfb7dde0e2d2f4
pastelnata <ritabb04.05@gmail.com>
2025-11-01 23:01:00 +0100

    format code

--- backend/main.ts
+++ backend/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api');
   const port = Number(process.env.PORT || 3000);
-  await app.listen(port, "0.0.0.0");
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}/api`);
 }
 
